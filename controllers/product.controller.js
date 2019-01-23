533a659cf47a33d1ca2b05634428aba85f0be145
Thor Anderson <ta.anderson@gmail.com>
2019-01-23 11:40:39 -0700

    Added create and update

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,22 +1,31 @@
 const Product = require('../models/product.model')
 
 // simple version with no validation or sanitation
-exports.test = function (req, res) {
-    res.send('Greetings from the test controller!')
+exports.test = (req, res) => {
+  res.send('Greetings from the test controller!')
 }
 
-exports.product_create = function (req, res, next) {
-    let product = new Product(
-        {
-            name: req.body.name,
-            price: req.body.price
-        }
-    )
+exports.product_create = (req, res, next) => {
+  let product = new Product({
+    name: req.body.name,
+    price: req.body.price,
+  })
 
-    product.save(function (err) {
-        if (err) {
-            return next(err)
-        }
-        res.send('Product created successfully')
-    })
+  product.save(function(err) {
+    if (err) {
+      return next(err)
+    }
+    res.send('Product created successfully')
+  })
+}
+
+exports.product_update = (req, res, next) => {
+  Product.findByIdAndUpdate(
+    req.params.id,
+    { $set: req.body },
+    (err, product) => {
+      if (err) return next(err)
+      res.send('Product updated.')
+    },
+  )
 }
--- routes/product.route.js
+++ routes/product.route.js
@@ -7,4 +7,6 @@ router.get('/test', product_controller.test)
 
 router.post('/create', product_controller.product_create)
 
+router.put('/:id/update', product_controller.product_update)
+
 module.exports = router
