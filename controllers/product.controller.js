70cdb4f59ee2f0050941b27cdd795cc6ac50bccd
Thor Anderson <ta.anderson@gmail.com>
2019-01-23 11:06:14 -0700

    Working createProduct endpoint

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,5 +2,21 @@ const Product = require('../models/product.model')
 
 // simple version with no validation or sanitation
 exports.test = function (req, res) {
-    res.send('Greetins from the Test controller!')
+    res.send('Greetings from the test controller!')
+}
+
+exports.product_create = function (req, res, next) {
+    let product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    )
+
+    product.save(function (err) {
+        if (err) {
+            return next(err)
+        }
+        res.send('Product created successfully')
+    })
 }
--- routes/product.route.js
+++ routes/product.route.js
@@ -5,4 +5,6 @@ const product_controller = require('../controllers/product.controller')
 
 router.get('/test', product_controller.test)
 
+router.post('/create', product_controller.product_create)
+
 module.exports = router
